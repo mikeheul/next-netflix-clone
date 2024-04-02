11b17a456d798dd75c994277b33d78c55798e6d8
Micka <mickael.murmann@gmail.com>
2024-04-02 17:12:00 +0200

    Force darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div className="m-5">
+      <Button>Hello Button</Button>
+    </div>
   );
 }
