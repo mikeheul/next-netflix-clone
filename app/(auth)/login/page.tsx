e38b46bdf1fdafe02d0ad88cd21304776e6b74cf
Micka <mickael.murmann@gmail.com>
2024-04-04 01:24:40 +0200

    Verify if logged user -> redirections

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { GithubIcon } from "lucide-react";
-import GoogleIcon from "../../../public/google.svg";
-import Image from "next/image";
 import Link from "next/link";
-import { signIn } from "next-auth/react";
 import GithubSignInButton from "@/app/components/GithubSignInButton";
 import GoogleSignInButton from "@/app/components/GoogleSignInButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../utils/auth"
+import { redirect } from "next/navigation";
 
-const Login = () => {
+const Login = async () => {
+    const session = await getServerSession(authOptions)
+    if(session) {
+        return redirect("/home")
+    }
+
     return ( 
         <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
             <form>
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,13 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { GithubIcon } from "lucide-react";
-import GoogleIcon from "../../../public/google.svg";
 import Link from "next/link";
-import Image from "next/image";
 import GithubSignInButton from "@/app/components/GithubSignInButton";
 import GoogleSignInButton from "@/app/components/GoogleSignInButton";
+import { authOptions } from "../../utils/auth"
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const SignUp = () => {
+const SignUp = async () => {
+    const session = await getServerSession(authOptions)
+    if(session) {
+        return redirect("/home")
+    }
+
     return ( 
         <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
             <form>
