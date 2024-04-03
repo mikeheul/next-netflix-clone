22c05115b6e29e552fff39b91471322fbfe5e80d
Micka <mickael.murmann@gmail.com>
2024-04-03 12:02:11 +0200

    Customize sign up form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
 const SignUp = () => {
     return ( 
-        <h1>SignUp</h1>
+        <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+            <form>
+                <h1 className="text-3xl font-semibold text-white">Sign up</h1>
+                <div className="space-y-4 mt-5">
+                    <Input 
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+                    />
+                    <Button
+                        type="submit"
+                        className="w-full bg-[#e50914]"
+                        variant="destructive"
+                    >
+                        Sign Up
+                    </Button>
+                </div>
+            </form>
+
+            <div className="text-gray-500 text-sm mt-2">
+                Already have an account ? <Link className="text-white hover:underline" href="/login">Log in now !</Link>
+            </div>
+        </div>
     );
 }
 
