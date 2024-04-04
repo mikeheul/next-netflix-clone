8091d2e9868dc944e6bd58dac60c810091c2c996
Micka <mickael.murmann@gmail.com>
2024-04-04 15:34:20 +0200

    MovieButtons component for homepage and change movie id for home movie video

--- app/components/MovieVideo.tsx
+++ app/components/MovieVideo.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 import prisma from "../utils/db";
+import MovieButtons from "./MovieButtons";
 
 async function getData() {
 
-    const data = await prisma.movie.findFirst({
+    const data = await prisma.movie.findUnique({
         select: {
             title: true,
             overview: true,
@@ -13,7 +14,11 @@ async function getData() {
             duration: true,
             id: true,
             age: true,
+            youtubeString: true
         },
+        where: {
+            id: 5
+        }
     })
 
     return data;
@@ -37,8 +42,16 @@ const MovieVideo = async () => {
                 <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold">{data?.title}</h1>
                 <p className="text-white text-lg mt-5 line-clamp-3">{data?.overview}</p>
                 <div className="flex gap-x-3 mt-4">
-                    <Button>See More</Button>
-                    <Button>Learn More</Button>
+                <MovieButtons
+                    age={data?.age as number}
+                    duration={data?.duration as number}
+                    id={data?.id as number}
+                    overview={data?.overview as string}
+                    releaseDate={data?.release as number}
+                    title={data?.title as string}
+                    youtubeUrl={data?.youtubeString as string}
+                    key={data?.id}
+                />
                 </div>
             </div>
         </div>
