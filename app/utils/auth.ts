5bf9648a876be20df7a4793bc1cde120a5a9e998
Micka <mickael.murmann@gmail.com>
2024-04-05 00:09:03 +0200

    Fix bug Adapter Auth

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -4,9 +4,10 @@ import prisma from "./db";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";  
+import { Adapter } from "next-auth/adapters";
 
 export const authOptions = {
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
