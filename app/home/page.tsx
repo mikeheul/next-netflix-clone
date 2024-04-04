a55ecc238b8286cc570ad1a43d95bdfa417b6539
Micka <mickael.murmann@gmail.com>
2024-04-04 14:29:47 +0200

    Recently added movies (MovieCard and MovieVideo components)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,13 @@
+import MovieVideo from "../components/MovieVideo";
 import Navbar from "../components/Navbar"
+import RecentlyAdded from "../components/RecentlyAdded";
 
 const HomePage = () => {
     return ( 
         <div className="p-5 lg:p-0">
+            <MovieVideo />
             <h1 className="text-3xl font-bold ">Recently Added</h1>
-      
+            <RecentlyAdded />
         </div>
     );
 }
